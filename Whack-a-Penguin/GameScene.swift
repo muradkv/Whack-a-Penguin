4f9aaba9c33a322bd8e82cc82c543bebbe50768c
murad <muradkv@gmail.com>
2019-06-12 22:46:20 +0300

    Initial commit

---
+++ Whack-a-Penguin/GameScene.swift
@@ -0,0 +1,20 @@
+//
+//  GameScene.swift
+//  Whack-a-Penguin
+//
+//  Created by murad on 12/06/2019.
+//  Copyright © 2019 murad. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+    }
+    
+}
