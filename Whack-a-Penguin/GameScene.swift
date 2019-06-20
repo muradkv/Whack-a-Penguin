452310e0b4afd1bfd1ce48e1a10b6bc9e831fcb1
murad <muradkv@gmail.com>
2019-06-20 23:20:25 +0300

    Add creating enemy

--- Whack-a-Penguin/GameScene.swift
+++ Whack-a-Penguin/GameScene.swift
@@ -18,6 +18,7 @@ class GameScene: SKScene {
             gameScore.text = "Score: \(score)"
         }
     }
+    var popupTime = 0.85
     
     override func didMove(to view: SKView) {
         let background = SKSpriteNode(imageNamed: "whackBackground")
@@ -37,6 +38,8 @@ class GameScene: SKScene {
         for i in 0 ..< 4 { createSlot(at: CGPoint(x: 180 + (i * 170), y: 320)) }
         for i in 0 ..< 5 { createSlot(at: CGPoint(x: 100 + (i * 170), y: 230)) }
         for i in 0 ..< 4 { createSlot(at: CGPoint(x: 180 + (i * 170), y: 140)) }
+        
+        createEnemy()
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -49,4 +52,23 @@ class GameScene: SKScene {
         slots.append(slot)
     }
     
+    func createEnemy() {
+        popupTime *= 0.991
+        
+        slots.shuffle()
+        slots[0].show(hideTime: popupTime)
+        
+        if Int.random(in: 0...12) > 4 { slots[1].show(hideTime: popupTime) }
+        if Int.random(in: 0...12) > 8 {  slots[2].show(hideTime: popupTime) }
+        if Int.random(in: 0...12) > 10 { slots[3].show(hideTime: popupTime) }
+        if Int.random(in: 0...12) > 11 { slots[4].show(hideTime: popupTime)  }
+        
+        let minDelay = popupTime / 2.0
+        let maxDelay = popupTime * 2
+        let delay = Double.random(in: minDelay...maxDelay)
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + delay) { [weak self] in
+            self?.createEnemy()
+        }
+    }
 }
