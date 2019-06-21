0c25e3959cedfbf12f452717c25bcf3b22f187a7
murad <muradkv@gmail.com>
2019-06-21 23:42:46 +0300

    Add hit penguin method

--- Whack-a-Penguin/WhackSlot.swift
+++ Whack-a-Penguin/WhackSlot.swift
@@ -62,4 +62,12 @@ class WhackSlot: SKNode {
         isVisible = false
     }
     
+    func hit() {
+        isHit = true
+        
+        let delay = SKAction.wait(forDuration: 0.25)
+        let hide = SKAction.moveBy(x: 0, y: -80, duration: 0.5)
+        let notVisible = SKAction.run { [unowned self] in self.isVisible = false }
+        charNode.run(SKAction.sequence([delay, hide, notVisible]))
+    }
 }
